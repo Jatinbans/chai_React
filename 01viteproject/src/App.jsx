29e4a2cc16e15b117d80764584a7d9e53486c6f3
jatinbans <jatinbansal80173@gmail.com>
2024-08-07 01:10:05 +0530

    understand custom react and other important points

--- 01viteproject/src/App.jsx
+++ 01viteproject/src/App.jsx
@@ -1,9 +1,14 @@
 import Chai from "./Chai.jsx"
 //component aa html retrun krda
 function App() {
+    const username = "chai aur code"
 
   return (
+    <>
    <Chai/>
+   <h1>chai aur react {username}</h1>
+   <p>para</p>
+   </>
   )
 }
 
--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -3,8 +3,43 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+  return(
+    <div>
+      <h1>custom app</h1>
+    </div>
+  )
+}
+
+// eda syntax shi nhi ga ta nhi chl reha eh kyoki ehnu apa customised bnaya
+/*
+const ReactElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
+*/
+
+const anotherElement = (
+  <a href="https://google.com" target='_google'>Visit google</a>
+)
+
+const anotherUser = "chai aur react"
+
+//tagname,props,childrentext
+const ReactElement = React.createElement(
+  'a',
+  {href:'https:google.com',targte:'_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    ReactElement
+
  
 )
