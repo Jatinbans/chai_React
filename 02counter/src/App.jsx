9d489f74afde8f61fc0eef5d14e2dd2133710776
jatinbans <jatinbansal80173@gmail.com>
2024-08-07 01:34:14 +0530

    minor functionalities added

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,11 +9,17 @@ function App() {
   const [counter,setCounter] = useState(15)
 
   const addValue= ()=>{
+    if(counter<20){
     setCounter(counter + 1)
+    }
   }
 
   const removeValue = ()=>{
+    if(counter > 0){
     setCounter(counter-1)
+    }
+   
+      
   }
   
   return (
