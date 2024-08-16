2c99be6fbb8218dcb5dc073fea76b249d76838ba
jatinbans <jatinbansal80173@gmail.com>
2024-08-16 13:09:14 +0530

    react-router-dom project

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,29 +1,57 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import './App.css'
 
 import './index.css'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import { Route } from 'react-router-dom'
+import React from 'react'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
 
 //createBrowserRouter method aa ik react router da jide andr array aa te ode andr list of object needed
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout />,
-    children:[
-      {
-        path:"",
-        element:<Home />
-      },
-      {
-        path:"about",
-        element:<About />
-      },
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout />,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home />
+//       },
+//       {
+//         path:"about",
+//         element:<About />
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact />
+//       },
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route loader={githubInfoLoader}
+      path='github/' 
+      element={<Github/>}/>
+
+    </Route>
+  )
+)
+
+
 createRoot(document.getElementById('root')).render(
   // RouterProvider ik component aa jhera props lenda bina props to km nhi krda
   <StrictMode>
